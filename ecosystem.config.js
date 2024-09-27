e99cd120209e620251d52699cf816d06ec79b3c8
Abhishek kumar <41527391+Invens@users.noreply.github.com>
2024-09-27 19:10:10 +0530

    Update ecosystem.config.js

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,9 +1,28 @@
 module.exports = {
   apps: [
     {
-      name: 'dealbot',
-      script: 'app.py',
-      interpreter: '/home/coupleup-deal/htdocs/deal.coupleup.in/amazon-affiliate-automate/venv/bin/python', // Virtual env Python
-    }
-  ]
+      name: 'dealbot', // First application
+      script: '/home/coupleup-deal/htdocs/deal.coupleup.in/amazon-affiliate-automate/app.py',
+      interpreter: '/home/coupleup-deal/htdocs/deal.coupleup.in/amazon-affiliate-automate/venv/bin/python',
+      watch: true,
+      env: {
+        FLASK_ENV: 'development',
+      },
+      env_production: {
+        FLASK_ENV: 'production',
+      },
+    },
+    {
+      name: 'scheduler', // Second application
+      script: '/home/coupleup-deal/htdocs/deal.coupleup.in/amazon-affiliate-automate/campaign_scheduler.py',
+      interpreter: '/home/coupleup-deal/htdocs/deal.coupleup.in/amazon-affiliate-automate/venv/bin/python',
+      watch: true,
+      env: {
+        FLASK_ENV: 'development',
+      },
+      env_production: {
+        FLASK_ENV: 'production',
+      },
+    },
+  ],
 };
