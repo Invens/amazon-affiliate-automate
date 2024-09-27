c95ffd7d7a008d344f8989f01ad320a1c669821e
Abhishek kumar <41527391+Invens@users.noreply.github.com>
2024-09-27 18:46:31 +0530

    Create ecosystem.config.js

---
+++ ecosystem.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  apps: [
+    {
+      name: 'dealbot',
+      script: 'app.py',
+      interpreter: '/home/coupleup-deal/htdocs/deal.coupleup.in/amazon-affiliate-automate/venv/bin/python', // Virtual env Python
+    }
+  ]
+};
